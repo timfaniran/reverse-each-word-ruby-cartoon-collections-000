b61ec04a65b6f5c346b00ccf0f5a5890e5b43437
Timothy Faniran <tim.faniran@gmail.com>
2019-05-12 23:43:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(string)
   arr = string.split(" ")
   new_arr = []
   arr.collect do |str|
-    new_arr << str.reverse
-  
+    new_arr << str.reverse_each_word
+    end
+  new_arr.join(" ")
 end
