6240b287fc89591604bf4d2cb49a6e25e61813dd
Timothy Faniran <tim.faniran@gmail.com>
2019-05-12 23:49:04 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,17 @@
+def reverse_each_word(string)
+  arr = string.split(" ")
+  new_arr = []
+  arr.collect do |str|
+    new_arr << str.reverse
+    end
+  new_arr.join(" ")
+end
+
+# def reverse_each_word_each(strg)
+#   array = strg.split(" ")
+#   new_array = []
+#   arr.each do |str|
+#     new_array << str.reverse
+#     end
+#   new_array.join(" ")
+# end
